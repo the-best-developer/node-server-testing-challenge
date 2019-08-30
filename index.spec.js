33996f3e5bfc9ca106608b03e653f58ffda4a63c
the-best-developer <im.dylan.gosnell@gmail.com>
2019-08-30 19:03:30 -0400

    Added tests for endpoints

--- index.spec.js
+++ index.spec.js
@@ -1,22 +1,40 @@
 const request = require('supertest');
 const server = require('./index.js'); 
 
+// Describing index.js endpoints
 describe('index.js', () => {
-describe('index route', () => {
-    it('should return an OK status code from the index route', async () => {
-      const response = await request(server).get('/');
-      expect(response.status).toEqual(200);
+
+  // POST endpoint
+  describe('POST /', () => {
+    it('should return a 200 status', async () => {
+      const response = await request(server)
+      .post('/')
+      .send({name: "Lord of the flies", desc: "Children shipwreak on an island"})
+      expect(response.status).toEqual(201);
     });
 
-    it('should return a JSON object fron the index route', async () => {
-      const response = await request(server).get('/');
-      expect(response.body).toEqual({message: "hey!!"});
+    it('should return a object from the / endpoint', async () => {
+      const response = await request(server)
+      .post('/')
+      .send({name: "Lord of the flies", desc: "Children shipwreak on an island"})
+      expect(typeof response.body).toEqual('object');
+    });
+  });
+
+  // DELETE endpoint
+  describe('DELETE /', () => {
+    it('should return a 200 status', async () => {
+      const response = await request(server)
+      .delete('/')
+      .send("1")
+      expect(response.status).toEqual(201);
     });
 
-    it('should return a JSON object fron the index route', async () => {
-      const response = await request(server).get('/');
-
-      expect(response.type).toEqual('application/json');
+    it('should return the deleted object from the / endpoint', async () => {
+      const response = await request(server)
+      .delete('/')
+      .send(1)
+      expect(typeof response.body).toEqual('object');
     });
   });
 });
