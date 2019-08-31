66c1c05514ece7c1d02acee71db8c2a26bfa8f21
the-best-developer <im.dylan.gosnell@gmail.com>
2019-08-30 23:51:20 -0400

    Finish tests for GET, POST, and DELETE endpoints

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require('express');
 const server = express();
+const booksDb = require('./database/books.js')
 
 server.use(express.json());
 
-server.get("/", (req, res) => {
-    return res.status(200).json({ message: "hey!!" })
+server.get("/", async (req, res) => {
+    return await res.status(200).send( await booksDb('books'))
 })
 
-server.post("/books", (req, res) => {
-    const { book, desc } = req.body;
-    return res.status(201).json({ message: "hey!!" })
+server.post("/book", async (req, res) => {
+    const {book, desc} = req.body;
+
+    try {
+        const addBook = await booksDb("books").insert({ book: book, desc: desc })
+        const addedBook = await booksDb("books").where({ id: addBook[0]});
+        res.status(200).json(addedBook)
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 })
+
+server.delete('/book/:id', async (req, res) => {
+    const {id} = req.params;
+
+    try {
+        
+        const result = await booksDb('books').where({ id: id }).del();
+        (result)
+            ?
+                res.status(200).json({ message: `Book ${id} has been deleted` })
+            :
+                res.status(200).json({ message: `Book ${id} could not be deleted` })
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
 
 module.exports = server;
--- index.spec.js
+++ index.spec.js
@@ -4,37 +4,47 @@ const server = require('./index.js');
 // Describing index.js endpoints
 describe('index.js', () => {
 
-  // POST endpoint
-  describe('POST /', () => {
+  describe('GET function on /', () => {
     it('should return a 200 status', async () => {
-      const response = await request(server)
-      .post('/')
-      .send({name: "Lord of the flies", desc: "Children shipwreak on an island"})
-      expect(response.status).toEqual(201);
+      const response = await request(server).get('/')
+      expect(response.status).toEqual(200);
     });
 
     it('should return a object from the / endpoint', async () => {
-      const response = await request(server)
-      .post('/')
-      .send({name: "Lord of the flies", desc: "Children shipwreak on an island"})
+      const response = await request(server).get('/')
       expect(typeof response.body).toEqual('object');
     });
   });
 
-  // DELETE endpoint
-  describe('DELETE /', () => {
+  // POST endpoint on /book
+  describe('POST function on /book', () => {
     it('should return a 200 status', async () => {
       const response = await request(server)
-      .delete('/')
-      .send("1")
-      expect(response.status).toEqual(201);
+      .post('/book')
+      .send({book: "Lord of the flies", desc: "Children shipwreak on an island"})
+      expect(response.status).toEqual(200);
+    });
+
+    it('should return a object from the /book endpoint', async () => {
+      const response = await request(server)
+      .post('/book')
+      .send({book: "Lord of the flies", desc: "Children shipwreak on an island"})
+      expect(typeof response.body).toEqual('object');
+    });
+  });
+
+  // DELETE endpoint on /
+  describe('DELETE function on /', () => {
+    it('should return a 200 status', async () => {
+      const response = await request(server)
+      .delete('/book/1')
+      expect(response.status).toEqual(200);
     });
 
     it('should return the deleted object from the / endpoint', async () => {
       const response = await request(server)
-      .delete('/')
-      .send(1)
-      expect(typeof response.body).toEqual('object');
+      .delete('/book/2')
+      expect(!!response.body.message).toEqual(true);
     });
   });
 });
